4199bc68e27d968101fc4ed03d07ad676ee0db63
aaron-sim <simaaron322@hotmail.com>
2023-05-09 15:23:48 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ resource "aws_key_pair" "hashicat" {
   public_key = tls_private_key.hashicat.public_key_openssh
 }
 
-module "s3_bucket" {
+module "s3_bucket-1" {
   source              = "app.terraform.io/aaron-terraform-enterprise/s3-bucket/aws"
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
